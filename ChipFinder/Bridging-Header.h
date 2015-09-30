c7377aaf38ce476a6efb1774852bbd26726de900
Benedikt Terhechte <terhechte@gmail.com>
2015-09-30 20:59:08 +0200

    Initial, working, commit

---
+++ ChipFinder/Bridging-Header.h
@@ -0,0 +1,21 @@
+//
+//  Bridging-Header.h
+//  ChipFinder
+//
+//  Created by Benedikt Terhechte on 30/09/15.
+//  Copyright © 2015 Benedikt Terhechte. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#include <CoreFoundation/CoreFoundation.h>
+#if __cplusplus
+extern "C" {
+#endif
+    CFPropertyListRef MGCopyAnswer(CFStringRef property);
+#if __cplusplus
+}
+#endif
+
+#endif /* Bridging_Header_h */
--- ChipFinder/ViewController.swift
+++ ChipFinder/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        guard let chipInfo = MGCopyAnswer("HardwarePlatform")
+            else { fatalError("Could not read hardware") }
+        
+        switch chipInfo.takeRetainedValue() as? String {
+            case "s8000"?:
+            self.label.text = "Samsung"
+            case "s8003"?:
+            self.label.text = "TSMC"
+        default:
+            self.label.text = "Unknown"
+        }
     }
 
     override func didReceiveMemoryWarning() {
