bcb52c0fadfcd1488e2911abe2a368b1278ef3f2
Benedikt Terhechte <terhechte@gmail.com>
2015-09-30 20:37:39 +0200

    Initial Commit

---
+++ ChipFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChipFinder
+//
+//  Created by Benedikt Terhechte on 30/09/15.
+//  Copyright © 2015 Benedikt Terhechte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
